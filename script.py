247bd53ccd672032187daf55d05778d686c0399b
Larry Gray <lwgray@gmail.com>
2019-03-24 06:46:05 +0000

    General commit

---
+++ script.py
@@ -0,0 +1,29 @@
+#importing libraries
+import os
+import numpy as np
+import flask
+import pickle
+from flask import Flask, render_template, request
+from sklearn.externals import joblib
+import pandas as pd
+
+#creating instance of the class
+app=Flask(__name__)
+
+#to tell flask what url shoud trigger the function index()
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/getdelay',methods=['POST','GET'])
+def get_delay():
+    if request.method == 'POST':
+        result = request.form
+        # logmodel = joblib.load('datascience.xz')
+        prediction = logmodel.predict(pd.Series([result['query']]))
+        return render_template('result.html', prediction=prediction)
+
+if __name__ == '__main__':
+    logmodel = joblib.load('datascience.xz')
+    app.run(debug=True)
